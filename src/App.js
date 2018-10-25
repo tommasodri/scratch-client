6b29ee9bb56314ea31c0e8d6b46bcedf35e3b2c8
Tommaso Dri <tommaso.dri@gmail.com>
2018-10-25 14:24:12 +0200

    Adding settings page

--- src/App.js
+++ src/App.js
@@ -42,41 +42,46 @@ class App extends Component {
     }
 
     render() {
-        const childProps = {
-            isAuthenticated: this.state.isAuthenticated,
-            userHasAuthenticated: this.userHasAuthenticated
-        };
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
 
-        return (
-            !this.state.isAuthenticating &&
-            <div className="App container">
-              <Navbar fluid collapseOnSelect>
-                <Navbar.Header>
-                  <Navbar.Brand>
-                    <Link to="/">Scratch</Link>
-                  </Navbar.Brand>
-                  <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                  <Nav pullRight>
-                    {this.state.isAuthenticated
-                      ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
-                      : <Fragment>
-                          <LinkContainer to="/signup">
-                            <NavItem>Signup</NavItem>
-                          </LinkContainer>
-                          <LinkContainer to="/login">
-                            <NavItem>Login</NavItem>
-                          </LinkContainer>
-                        </Fragment>
-                    }
-                  </Nav>
-                </Navbar.Collapse>
-              </Navbar>
-              <Routes childProps={childProps} />
-            </div>
-        );
-    }
+    return (
+      !this.state.isAuthenticating &&
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {this.state.isAuthenticated
+                ? <Fragment>
+                    <LinkContainer to="/settings">
+                      <NavItem>Settings</NavItem>
+                    </LinkContainer>
+                    <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                  </Fragment>
+                : <Fragment>
+                    <LinkContainer to="/signup">
+                      <NavItem>Signup</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                      <NavItem>Login</NavItem>
+                    </LinkContainer>
+                  </Fragment>
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
+      </div>
+    );
+  }
 }
 
 export default withRouter(App);
